8648e6797599de6c5b2aa32bf9b50fd95fa542ab
Kumaran-gsk <115685300+Kumaran-gsk@users.noreply.github.com>
2023-08-24 15:49:32 +0530

    Update testRunner.java

--- src/test/java/runner/testRunner.java
+++ src/test/java/runner/testRunner.java
@@ -14,11 +14,11 @@ import io.cucumber.junit.CucumberOptions;
 //	tags = ("@1Login"),
 	tags = ("@1Login or @2smoke or @Designation or @AddEmployee or @CreateProject or @Announcement or @Feedback or @SelfReport or @Report or @LeaveReport" ),
 	
-        plugin = {"pretty","html:test-output"}
-//	plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
-//			"rerun:target/Firstfailedrerun.txt"}
-//	plugin = { "pretty", 
-		       //  "html:target/cucumber-reports" , "json:target/report.json" }
+         plugin = {
+        "pretty",
+        "json:target/cucumber-reports/CucumberTestReport.json",
+        "html:target/cucumber-reports/html-report"
+        }
 	)
 
 public class testRunner  {
